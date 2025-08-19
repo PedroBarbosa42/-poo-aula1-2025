6736c03b8a59dd32e4f04277a79a1bd364e5a22f
Barbosa <pedrocombr93@gmail.com>
2025-08-19 01:42:19 +0000

    AULA 18.08

--- src/main/java/app/Aluno.java
+++ src/main/java/app/Aluno.java
@@ -1,7 +1,17 @@
 package app;
 
-public class Aluno{
-    String nome;
-    int idade;
+public class Aluno {
+    public String nome;
+    public int idade;
 
+    public String getDados() {
+        return this.nome + "(" + this.idade + ")";
+    }
+
+    public Aluno(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Aluno() {}
 }
--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,33 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("Ola mundo!!!");
+        Aluno a1 = new Aluno();
+        a1.nome = "Pedro";
+        a1.idade = 18;
+
+        Aluno a2 = a1;
+        a2.nome = "Marcos";
+
+        System.out.println("[A1]");
+        System.out.println("nome = " + a1.nome);
+        System.out.println("idade = " + a1.idade);
+
+        System.out.println("[A2]");
+        System.out.println("nome = " + a2.nome);
+        System.out.println("idade = " + a2.idade);
+
+        System.out.println("[ CHAMADA DE MÉTODO ]");
+        System.out.println(" A1 = " + a1.getDados());
+        System.out.println(" A1 = " + a2.getDados());
+
+        Aluno a3 = new Aluno("MARIA", 35);
+        System.out.println("[ CHAMADA DE MÉTODO ]");
+        System.out.println(" A3 = " + a3.getDados());
+
+        Cliente c1 = new Cliente();
+        c1.setNome("Teste");
+        System.out.println("[ CHAMADA DO GET ]");
+        System.out.println(c1.getNome());
+
     }
 }
